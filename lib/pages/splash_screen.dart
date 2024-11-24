6d44f7459e9eab91be56dbf4e097b8002e67f532
ganeshgovala <sriramganesh28@gmail.com>
2024-11-24 19:43:59 +0530

    Added Splash screen

---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: Center(
+              child: Image.asset("lib/assets/bunkit-logo.png"
+              ),
+            ),
+          )
+        ],
+      )
+    );
+  }
+}
